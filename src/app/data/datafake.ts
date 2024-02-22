ec143a97df0df69d38d7d4a102b21c64a8e9b9f1
Alan Johnny <johnnypontes711@gmail.com>
2024-02-22 20:59:12 -0300

    content created

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "RBR",
+    "description": "Max Verstappen e Sergio Pérez, dupla da equipe austríaca desde 2021, serão os pilotos a bordo do RB20. O modelo foi revelado nesta quinta-feira, encerrando as divulgações das pinturas de 2024, e não fugiu dos padrões anteriores da hexacampeã mundial - mantendo seu azul-marinho como cor principal.",
+    "photoCover":"https://s2-ge.glbimg.com/34saiixfDPY6-Dm6sVN6YH31lR8=/0x0:3840x2441/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2024/U/q/jrE0IuRQOKWGs4RNABgw/si202402140461.jpg"
+  }
+  ,
+  {
+    "id": "2",
+    "title": " Mercedes",
+    "description": "Nem totalmente preto, nem totalmente prata. A octacampeã de construtores aderiu ao melhor dos dois mundos para o W15, último modelo que Lewis Hamilton, de saída confirmada para a Ferrari, guiará no time alemão. Ele correrá ao lado de George Russell, seu colega desde 2022.",
+    "photoCover":"https://s2-ge.glbimg.com/QTm_HTdhZjZN10dPgkaFXvKPFwc=/0x0:2048x1152/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2024/s/C/ePrncUTjasiHwtXgBVIQ/whatsapp-image-2024-02-14-at-07.15.40.jpeg"
+
+ }
+ ,
+ {
+  "id": "3",
+  "title": "Ferrari",
+  "description": "O vermelho da escuderia jamais mudará, mas dessa vez, o SF24 veio acompanhado de alguns toques de branco e amarelo, substituindo o preto visto no modelo anterior. Será o último carro guiado pela dupla Carlos Sainz e Charles Leclerc, já que Sainz deixará o time ao fim do ano para dar lugar a Lewis Hamilton.",
+  "photoCover":"https://s2-ge.glbimg.com/he6aM0HRNi5f00DbF-eQKeTJwN0=/0x0:4000x2250/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2024/u/X/E2HFwhQKOWtbYzSvmlLw/ferrari-sf24-02.jpg"
+}
+,
+{
+  "id": "4",
+  "title": "McLaren",
+  "description": "Desde 2017, o laranja papaya tem sido a cor adotada pela McLaren para seu carro, e não será diferente com o MCL38 apesar do modelo ter mais fibra de carbono exposta em comparação com seu antecessor. A equipe saiu na frente da rivais e divulgou a pintura ainda em janeiro; o carro, de fato, foi revelado apenas nesta quarta-feira. Lando Norris e Oscar Piastri seguem pilotos do time.",
+  "photoCover":"https://s2-ge.glbimg.com/W_76M0SClvO5320TDMV_B0IAmgA=/0x0:1228x690/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2024/q/7/lefFJzQ8iCbKN9sBrKFg/ggstuqsxuaakfat.jpg"
+}
+]
